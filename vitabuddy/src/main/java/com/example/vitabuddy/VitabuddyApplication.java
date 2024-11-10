b942b1ce398975e2271404b51276311fa70d7b72
Dodan796 <dodan0825@gmail.com>
2024-11-10 13:40:57 +0900

    리뷰페이지네이션(수정삭제에러해결)+(리뷰작성&수정&삭제 가이드라인 추가)

--- vitabuddy/src/main/java/com/example/vitabuddy/VitabuddyApplication.java
+++ vitabuddy/src/main/java/com/example/vitabuddy/VitabuddyApplication.java
@@ -4,10 +4,19 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.example.vitabuddy" })
 @MapperScan(basePackages = { "com.example.vitabuddy" })
+@PropertySources({
+
+@PropertySource(value={"file: c:/springBootWorkspace/webservice /configure.properties",
+"file:/usr/local/project/properties/configure.properties", },
+ignoreResourceNotFound=true)
+
+})
 public class VitabuddyApplication {
 
     public static void main(String[] args) {
--- vitabuddy/src/main/resources/static/js/insertReview.js
+++ vitabuddy/src/main/resources/static/js/insertReview.js
@@ -8,13 +8,51 @@ $(document).ready(function () {
     var formData = new FormData(form);
     var files = $("#uploadFile")[0].files;
     var supId = $("input[name='supId']").val();
+    
+    //1108 정합성 검사 수정 폼
+    //1. 리뷰 제목
+    const reviewTitle = $("input[name='reviewTitle']").val().trim();
+    if (reviewTitle === "") {
+      alert("리뷰 제목을 작성해주세요.");
+      return;
+    }
+    
+    //2. 별점 선택
+    const selectedRating = $("input[name='rating']:checked").length
+    //console.log(selectedRating);  테스트 출력 : 별점을 선택하지 않고 "수정완료" 버튼을 누르면 길이가 0이라고 출력된다
+    if (selectedRating==0) {   //별점 length 가 0일 때
+      alert("별점을 입력해주세요.");
+      return;
+    }
+    
+    
+	//3. 해시태그
+    if (!$("#hashtag").val()) {
+      alert("해시태그를 선택해 주세요.");
+      return;
+    }
+
+	//4. 복용기간
+    const startDate = $("#date1").val();
+    const endDate = $("#date2").val();
+    if (!startDate || !endDate) {
+      alert("복용기간을 입력해주세요.");
+      return;
+    }
+    
+    
+    //5. 리뷰 컨텐츠
+    const reviewContent = $("textarea[name='content']").val().trim();
+    if (reviewContent === "") {
+      alert("리뷰를 작성해주세요.");
+      return;
+    }
+    
+    //1108-------------------------
 
     // 사진 3장 제한 체크 => 3장 초과 시 더 이상 진행하지 않음
     if (files.length > 3) {
       alert("사진은 최대 3장까지 업로드 가능합니다.");
-      return;
-    } else if (files.length < 1){ //=> 최소 한장의 이미지 업로드를 위한 코드 추가 11.05 .
-    	alert("최소 1장의 사진을 업로드 하세요.");
       return;
     }
 
